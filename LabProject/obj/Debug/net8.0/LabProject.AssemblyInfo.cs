d4950c58b8885d5e877d02a64e2b8d3e59749a74
Ege Arca Tunç <c2211067@student.cankaya.edu.tr>
2025-04-03 14:35:59 +0300

    Week 6 branch pushed.

--- LabProject/obj/Debug/net8.0/LabProject.AssemblyInfo.cs
+++ LabProject/obj/Debug/net8.0/LabProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LabProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c431a4c05dbf5c662a7c162a9e19a83fcc88bd6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca4c6610a7eb3ba483afc965df09209823d7069f")]
 [assembly: System.Reflection.AssemblyProductAttribute("LabProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LabProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
